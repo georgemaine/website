6693dc03518e619e1de0b1a941d964edca814aef
Georgemaine <georgemaine.lourens@gmail.com>
2021-09-11 14:48:09 +0200

    Added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import "../components/globals.css";
+import Head from "next/head";
 
 function Website({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel='icon' href='favicon.svg' />
+        <link rel='mask-icon' href='mask-icon.svg' color='var(--black)' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default Website;
