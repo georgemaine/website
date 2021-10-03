b901c206f7f0916594786ba426efce57ed28e255
Georgemaine <georgemaine.lourens@gmail.com>
2021-10-03 12:10:34 +0200

    Debug 18

--- pages/get-in-touch.js
+++ pages/get-in-touch.js
@@ -49,54 +49,50 @@ export default function GetInTouch() {
             <main>
               <Head />
               <GlobalNav />
+              {/* 
               <StickyMediaTile
                 src={"georgemaine.jpeg"}
                 alt={"Georgemaine Lourens"}
                 margin={"0 0 9.5rem"}
-              />
-              {imagesLoaded ? (
-                <>
-                  <TextWithTransition>
-                    As you may or may not be aware, I’ve always been fascinated
-                    with how things work.
-                  </TextWithTransition>
+              /> */}
+              <TextWithTransition>
+                As you may or may not be aware, I’ve always been fascinated with
+                how things work.
+              </TextWithTransition>
 
-                  <TextWithTransition>
-                    Could I remake it differently? Would people prefer that?
-                  </TextWithTransition>
-                  <TextWithTransition>
-                    The part I hated was showing my version to people — Go
-                    figure.
-                  </TextWithTransition>
-                  <TextWithTransition>
-                    Currently, I’m an indoor gardener and product designer at{" "}
-                    <InlineLink href={"https://pitch.io"} text={"Pitch"} />
-                  </TextWithTransition>
-                  <TextWithTransition>
-                    I love surrounding myself with extraordinary people and
-                    plants — they teach me so much and make me a better person.
-                  </TextWithTransition>
-                  <TextWithTransition>
-                    If you love that too, reach out via{" "}
-                    <InlineLink
-                      href={
-                        "mailto:georgemaine.lourens@gmail.com?subject=Hello%20%F0%9F%91%8B"
-                      }
-                      text={"Email"}
-                    />
-                    ,
-                    <InlineLink
-                      href={"https://twitter.com/georgemaine"}
-                      text={"Twitter"}
-                    />
-                    , or{" "}
-                    <InlineLink
-                      href={"https://www.linkedin.com/in/georgemaine"}
-                      text={"LinkedIn"}
-                    />
-                  </TextWithTransition>
-                </>
-              ) : null}
+              <TextWithTransition>
+                Could I remake it differently? Would people prefer that?
+              </TextWithTransition>
+              <TextWithTransition>
+                The part I hated was showing my version to people — Go figure.
+              </TextWithTransition>
+              <TextWithTransition>
+                Currently, I’m an indoor gardener and product designer at{" "}
+                <InlineLink href={"https://pitch.io"} text={"Pitch"} />
+              </TextWithTransition>
+              <TextWithTransition>
+                I love surrounding myself with extraordinary people and plants —
+                they teach me so much and make me a better person.
+              </TextWithTransition>
+              <TextWithTransition>
+                If you love that too, reach out via{" "}
+                <InlineLink
+                  href={
+                    "mailto:georgemaine.lourens@gmail.com?subject=Hello%20%F0%9F%91%8B"
+                  }
+                  text={"Email"}
+                />
+                ,
+                <InlineLink
+                  href={"https://twitter.com/georgemaine"}
+                  text={"Twitter"}
+                />
+                , or{" "}
+                <InlineLink
+                  href={"https://www.linkedin.com/in/georgemaine"}
+                  text={"LinkedIn"}
+                />
+              </TextWithTransition>
             </main>
           </div>
         </div>
