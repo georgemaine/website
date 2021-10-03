574cee879ac0747fb17eefe3607cf066b9bca1f9
Georgemaine <georgemaine.lourens@gmail.com>
2021-10-03 12:03:36 +0200

    Added images load check

--- components/utils.js
+++ components/utils.js
@@ -119,3 +119,35 @@ export const useOnScreen = (ref, rootMargin = "0px") => {
   }, [ref, rootMargin]);
   return isIntersecting;
 };
+
+export const onImagesLoaded = (images) => {
+  return new Promise((resolve) => {
+    if (!images.length) {
+      resolve();
+
+      return;
+    }
+
+    let loadedCount = 0;
+
+    const onComplete = () => {
+      ++loadedCount;
+
+      if (loadedCount === images.length) {
+        resolve();
+      }
+
+      this.removeEventListener("load", onComplete);
+      this.removeEventListener("error", onComplete);
+    };
+
+    images.forEach((img) => {
+      if (img.complete) {
+        onComplete.call(img);
+      } else {
+        img.addEventListener("load", onComplete);
+        img.addEventListener("error", onComplete);
+      }
+    });
+  });
+};
--- pages/get-in-touch.js
+++ pages/get-in-touch.js
@@ -3,8 +3,15 @@ import Head from "../components/Head";
 import { StickyMediaTile } from "../components/StickyMediaTile";
 import { TextWithTransition, InlineLink } from "../components/TextTile";
 import { ScrollBar } from "../components/ScrollBar";
+import { onImagesLoaded } from "../components/utils";
+import { useEffect, useState } from "react";
 
 export default function GetInTouch() {
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+  useEffect(() => {
+    const images = document.querySelectorAll("img");
+    onImagesLoaded(images).then(setImagesLoaded(true));
+  }, []);
   return (
     <div style={{ minHeight: "100vh" }}>
       <div
@@ -47,44 +54,49 @@ export default function GetInTouch() {
                 alt={"Georgemaine Lourens"}
                 margin={"0 0 9.5rem"}
               />
-              <TextWithTransition>
-                As you may or may not be aware, I’ve always been fascinated with
-                how things work.
-              </TextWithTransition>
+              {imagesLoaded ? (
+                <>
+                  <TextWithTransition>
+                    As you may or may not be aware, I’ve always been fascinated
+                    with how things work.
+                  </TextWithTransition>
 
-              <TextWithTransition>
-                Could I remake it differently? Would people prefer that?
-              </TextWithTransition>
-              <TextWithTransition>
-                The part I hated was showing my version to people — Go figure.
-              </TextWithTransition>
-              <TextWithTransition>
-                Currently, I’m an indoor gardener and product designer at{" "}
-                <InlineLink href={"https://pitch.io"} text={"Pitch"} />
-              </TextWithTransition>
-              <TextWithTransition>
-                I love surrounding myself with extraordinary people and plants —
-                they teach me so much and make me a better person.
-              </TextWithTransition>
-              <TextWithTransition>
-                If you love that too, reach out via{" "}
-                <InlineLink
-                  href={
-                    "mailto:georgemaine.lourens@gmail.com?subject=Hello%20%F0%9F%91%8B"
-                  }
-                  text={"Email"}
-                />
-                ,
-                <InlineLink
-                  href={"https://twitter.com/georgemaine"}
-                  text={"Twitter"}
-                />
-                , or{" "}
-                <InlineLink
-                  href={"https://www.linkedin.com/in/georgemaine"}
-                  text={"LinkedIn"}
-                />
-              </TextWithTransition>
+                  <TextWithTransition>
+                    Could I remake it differently? Would people prefer that?
+                  </TextWithTransition>
+                  <TextWithTransition>
+                    The part I hated was showing my version to people — Go
+                    figure.
+                  </TextWithTransition>
+                  <TextWithTransition>
+                    Currently, I’m an indoor gardener and product designer at{" "}
+                    <InlineLink href={"https://pitch.io"} text={"Pitch"} />
+                  </TextWithTransition>
+                  <TextWithTransition>
+                    I love surrounding myself with extraordinary people and
+                    plants — they teach me so much and make me a better person.
+                  </TextWithTransition>
+                  <TextWithTransition>
+                    If you love that too, reach out via{" "}
+                    <InlineLink
+                      href={
+                        "mailto:georgemaine.lourens@gmail.com?subject=Hello%20%F0%9F%91%8B"
+                      }
+                      text={"Email"}
+                    />
+                    ,
+                    <InlineLink
+                      href={"https://twitter.com/georgemaine"}
+                      text={"Twitter"}
+                    />
+                    , or{" "}
+                    <InlineLink
+                      href={"https://www.linkedin.com/in/georgemaine"}
+                      text={"LinkedIn"}
+                    />
+                  </TextWithTransition>
+                </>
+              ) : null}
             </main>
           </div>
         </div>
